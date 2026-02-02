ccf9aa7b018bde8c0a562a0735cf0f554e5c5ad2
faisal <mfaisalnoorzad@gmail.com>
2026-02-01 20:13:09 -0500

    Added collisions to the game.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -32,6 +33,11 @@ def main():
         screen.fill("black")
         dt = clock.tick(60) / 1000
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
